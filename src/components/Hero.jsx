187fe41a81b0d39166ab174d891038879dcfcdb7
Giancarlo Poem <giancarlo.poemape@hotmail.com>
2023-06-18 03:44:13 -0500

    creating computers component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} 
+        absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
+          <div className="flex flex-col justify-center items-center mt-5">
+            <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+            <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          </div>
+          <div>
+              <h1 className={`${styles.heroHeadText} text-white`}>
+                Hi, I'm <span className="text-[#915eff]">Giancarlo</span>
+              </h1>
+              <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                I have worked as a FullStack Developer, Mobile Developer, <br className="sm:block hidden" />
+                Data Engineer, and currently as a Machine Learning Engineer
+              </p>
+            </div>
+      </div>
+
+    <ComputersCanvas />
+
+    </section>
   )
 }
 
