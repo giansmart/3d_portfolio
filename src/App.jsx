3e0206344d9912bc49b53f92a4b6cb57bca6afe2
Giancarlo Poem <giancarlo.poemape@hotmail.com>
2023-06-27 17:57:16 -0500

    comenting projects

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
         <About />
         <Experience />
         <Tech />
-        <Works />
+        {/*<Works />*/}
         <Feedbacks />
         <div className="relative z-0">
           <Contact />
